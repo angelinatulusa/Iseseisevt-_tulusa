36a8d69e8bd16547a2af711aad591fbcc8e6fabf
opilane <opilane@HP-CZC6297BB4>
2022-02-23 12:54:13 +0200

    Add project files.

---
+++ 1ülesanne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtöö_tulusa
+{
+    class _1ülesanne
+    {
+        static void Main()
+        {
+
+            //Задано пять произвольных целых чисел (элементы массива). Определить, является ли их расположение в массиве упорядоченным (т.е. по возрастанию или по убыванию) или неупорядоченным.
+            string a = "";
+            int[] cisla = new int[5] { 18, 13, 32, 76, 54};
+            a = cisla.Sort();
+            
+            
+        }
+        private static void Sort(int[] cisla)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ 2ülesanne.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtöö_tulusa
+{
+    class _2ülesanne
+    {
+        static void Main()
+        {
+            //Задан массив действительных чисел из N элементов (используйте генератор случайных чисел). Определить количество элементов, значения которых находятся вне диапазона от -10 до +10.
+            int k = 0;
+            int n = 0;
+            int[] N_mass = new int[5];
+            while (k!=5)
+            {
+                Console.WriteLine("vvedi cislo");
+                n = int.Parse(Console.ReadLine());
+                N_mass.Add(n);
+                k++;
+            }
+            foreach (var item in N_mass)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+
+
+            /*int k = 0;
+            for (int i = n; i < 5; i++)
+            {
+                N_mass[n] = i;
+                k++;
+            }
+            foreach (var item in N_mass)
+            {
+                if (true)
+                {
+
+                }
+            }*/
+
+        }
+    }
+}
